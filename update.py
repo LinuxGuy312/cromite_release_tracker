245f97aaa625b0a3f23712d082b7ed0a553efe8f
Eren <101087324+LinuxGuy312@users.noreply.github.com>
2024-05-23 17:21:20 +0000

    feat: Show changelog in message. No changelog if no issue comment

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 BODY_REG = r"https://github\.com/uazo/cromite/issues/.*\b"
 
-dt = datetime.today().strftime('[%d-%m-%Y | %H:%M:%S UTC] : ')
+dt = datetime.today().strftime("[%d-%m-%Y | %H:%M:%S UTC] : ")
 
 url = "https://api.github.com/repos/uazo/cromite/releases/latest"
 
@@ -16,42 +16,64 @@ chats = [1413518510, -1001747127529, -1001552719792]
 def get_new_release():
     fn = "release.tag"
     req = requests.get(url).json()
-    new_release = req['tag_name']
-    with open(fn, 'r') as r:
+    new_release = req["tag_name"]
+    with open(fn, "r") as r:
         old_tag = r.read().strip()
     if old_tag == new_release:
         return (None, None)
-    html_url = req['html_url']
-    comment_url = re.findall(BODY_REG, req['body'])[0]
-    with open('releases.log', 'a+') as rl:
-        rl.write(dt + new_release + '\n')
-    with open(fn, 'w') as f:
+    html_url = req["html_url"]
+    try:
+        comment_url = re.findall(BODY_REG, req["body"])[0]
+    except IndexError:
+        comment_url = None
+    with open("releases.log", "a+") as rl:
+        rl.write(dt + new_release + "\n")
+    with open(fn, "w") as f:
         f.write(new_release)
     return (html_url, comment_url)
+
+
+def get_changelog(cmtu):
+    if not cmtu:
+        return None
+    cmtn = cmtu.split("/")[-1].split("#")[0]
+    api = f"https://api.github.com/repos/uazo/cromite/issues/{cmtn}/comments"
+    resp = requests.get(api).json()
+    changes = []
+    for i in resp:
+        if cmtu == i["html_url"]:
+            changes = i["body"].split("- ")[1:]
+            break
+    if not changes:
+        return f"see [#{cmtn}]({cmtu})"
+    changelog = "*CHANGES:*\n\n"
+    for i in changes:
+        changelog += f"• {i}"
+    return changelog
 
 
 def release():
     html, comm = get_new_release()
     if not html:
         return None
-    if not comm:
-        return None
-    cmtn = comm.split('/')[-1].split('#')[0]
-    dl = html.replace('/tag/', '/download/')
-    version = html.split('/')[-1].split('-')[0]
+    dl = html.replace("/tag/", "/download/")
+    version = html.split("/")[-1].split("-")[0]
+    changes = get_changelog(comm)
+    if changes:
+        changelog = f"\n{changes}\n`{'_'*20}`\n"
+    else:
+        changelog = ""
+
 
     text = f"""[Cromite {version}]({html})
 `{'_'*20}`
-    
-see [#{cmtn} (comment)]({comm})
-`{'_'*20}`
-    
+{changelog}
 *DOWNLOADS:*
-    
+
 [arm64_ChromePublic.apk]({dl}/arm64_ChromePublic.apk)
 
 [arm_ChromePublic.apk]({dl}/arm_ChromePublic.apk)
-    
+
 [x64_ChromePublic.apk]({dl}/x64_ChromePublic.apk)
 
 [chrome-win.zip]({dl}/chrome-win.zip)
@@ -70,12 +92,13 @@ def update():
         return
     for chat in chats:
         data = {
-            'chat_id': chat,
-            'text': new_release,
-            'parse_mode': "MARKDOWN",
-            'disable_web_page_preview': True
+            "chat_id": chat,
+            "text": new_release,
+            "parse_mode": "MARKDOWN",
+            "disable_web_page_preview": True,
         }
         requests.post(api_url, data=data)
     return
+
 
 update()
